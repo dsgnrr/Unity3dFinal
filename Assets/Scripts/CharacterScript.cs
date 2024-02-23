a8c82106de0516d5f09eecf11f0e533e4310130b
dsgnrr <semeniuk.oleg2002@gmail.com>
2024-02-23 19:20:57 +0200

    Task2 Jumping, bind camera to character

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,28 +4,64 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 100f;
-    private CharacterController character;
+    private float speed = 10f;
+    private float playerVelocityY;
+    private float gravityValue = -9.80f;
+    private float jumpHeight = 1.0f;
+    private bool groundedPlayer;
+
+    private CharacterController _characterController;
+
     void Start()
     {
-        character = GetComponent<CharacterController>();
+        _characterController = GetComponent<CharacterController>();
+        playerVelocityY = 0f;
+
     }
     void Update()
     {
-        float dx = Input.GetAxis("Horizontal");
-        float dy = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(dx) > 0 && Mathf.Abs(dy) > 0)
+        groundedPlayer = _characterController.isGrounded;
+        if (groundedPlayer && playerVelocityY < 0)
         {
-            dx *= 0.707f; 
-            dy *= 0.707f;
+            playerVelocityY = 0f;
         }
 
-        character.SimpleMove(speed *
-            Time.deltaTime *
-            (dx * Camera.main.transform.right +
-            dy * Camera.main.transform.forward)
-            );
+        float dx = Input.GetAxis("Horizontal");
+        float dy = Input.GetAxis("Vertical");
+        if (Mathf.Abs(dx) > 0 && Mathf.Abs(dy) > 0)
+        {
+            dx *= 0.707f;
+            dy *= 0.707f;
+        }
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocityY += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
+        Vector3 horizontalForward = Camera.main.transform.forward;
+        horizontalForward.y = 0;
+        horizontalForward = horizontalForward.normalized;
+        this.transform.forward = horizontalForward;
+
+        playerVelocityY += gravityValue * Time.deltaTime;
+
+        _characterController.Move(Time.deltaTime *
+            (speed * (dx * Camera.main.transform.right + dy * horizontalForward) +
+            playerVelocityY * Vector3.up));
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter: " + other.gameObject.name);
+        if (other.CompareTag("Floor"))
+        {
+            groundedPlayer = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit: " + other.gameObject.name);
+        if (other.CompareTag("Floor"))
+        {
+            groundedPlayer = false;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # FinalProject tasks
 
+## Task2: Jumping, Bind camera to character
+![alt](https://github.com/dsgnrr/Unity3dFinal/blob/main/Tasks/task2/result.gif)
+
 ## Task1: character moving. FPV character moving.
 ![alt](https://github.com/dsgnrr/Unity3dFinal/blob/main/Tasks/task1/result.gif)
