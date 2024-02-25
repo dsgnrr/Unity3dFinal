111f85a1da4cb81aaf314582d8988491f34ccda5
dsgnrr <semeniuk.oleg2002@gmail.com>
2024-02-25 18:41:01 +0200

    Task3: Character base animation

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -11,15 +11,17 @@ public class CharacterScript : MonoBehaviour
     private bool groundedPlayer;
 
     private CharacterController _characterController;
-
+    private Animator _animator; 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator=GetComponent<Animator>();
         playerVelocityY = 0f;
 
     }
     void Update()
     {
+        int animatorState = 0;
         groundedPlayer = _characterController.isGrounded;
         if (groundedPlayer && playerVelocityY < 0)
         {
@@ -37,6 +39,10 @@ public class CharacterScript : MonoBehaviour
         {
             playerVelocityY += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
+        if (dy != 0)
+        {
+            animatorState = 1;
+        }
         Vector3 horizontalForward = Camera.main.transform.forward;
         horizontalForward.y = 0;
         horizontalForward = horizontalForward.normalized;
@@ -47,6 +53,7 @@ public class CharacterScript : MonoBehaviour
         _characterController.Move(Time.deltaTime *
             (speed * (dx * Camera.main.transform.right + dy * horizontalForward) +
             playerVelocityY * Vector3.up));
+        _animator.SetInteger("State", animatorState);
     }
     private void OnTriggerEnter(Collider other)
     {
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # FinalProject tasks
 
+## Task3: Character base animation
+![alt](https://github.com/dsgnrr/Unity3dFinal/blob/main/Tasks/task3/result.gif)
+
 ## Task2: Jumping, Bind camera to character
 ![alt](https://github.com/dsgnrr/Unity3dFinal/blob/main/Tasks/task2/result.gif)
 
